d9a4c38d784820ee5d11d747c838e88d883d4002
csebisht93 <csebisht@gmail.com>
2020-01-23 21:36:57 +0530

    test

--- Artificial Natural Language.py
+++ Artificial Natural Language.py
@@ -1,9 +1,15 @@
 import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
-from nltk.tokenize import WordPuncttokenizer
 
+#https://www.tutorialspoint.com/artificial_intelligence_with_python/artificial_intelligence_with_python_nltk_package.htm
 larger = "We use the method word_tokenize() to split a sentence into words. "
 print(sent_tokenize(larger))
 print(word_tokenize(larger))
-print(WordPuncttokenizer())
+sentence=[("a","DT"),("clever","JJ"),("fox","NN"),("was","VBP"),
+          ("jumping","VBP"),("over","IN"),("the","DT"),("wall","NN")]
+grammar = "NP:{<DT>?<JJ>*<NN>}"
+parser_chunking = nltk.RegexpParser(grammar)
+parser_chunking.parse(sentence)
+Output_chunk = parser_chunking.parse(sentence)
+Output_chunk.draw()
